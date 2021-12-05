8216140a22be6851b2bf52260bc27acdb2fa1357
Rotherda <70058769+rotherda@users.noreply.github.com>
2021-12-05 22:15:38 +0300

    Create bigmac.sh

---
+++ bigmac.sh
@@ -0,0 +1,10 @@
+#!usr/bin/env bash
+interface=$(ifconfig | cut -f 1 -d " " | head -n 1 | sed 's/://')
+printf "Please write a mac address what you want:\n"
+read selectedmac
+function change {
+ifconfig $interface down
+ifconfig $interface hw ether $selectedmac
+ifconfig $interface up
+}
+change
