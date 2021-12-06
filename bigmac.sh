ee553f2f6faeed651de4ef8ea3d83a834216937d
Rotherda <70058769+rotherda@users.noreply.github.com>
2021-12-06 08:58:15 +0300

    Update bigmac.sh

--- bigmac.sh
+++ bigmac.sh
@@ -3,8 +3,10 @@ interface=$(ifconfig | cut -f 1 -d " " | head -n 1 | sed 's/://')
 printf "Please write a mac address what you want:\n"
 read selectedmac
 function change {
+service NetworkManager stop
 ifconfig $interface down
 ifconfig $interface hw ether $selectedmac
 ifconfig $interface up
+service NetworkManager start
 }
 change
